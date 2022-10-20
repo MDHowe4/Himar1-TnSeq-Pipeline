8373f622d17720a4ce3f1aecd4a7a984b8d5644b
Michael H <howex118@umn.edu>
2022-10-19 23:47:02 -0500

    init

---
+++ Tnseq_Analysis_Script.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=20
+#SBATCH --mem=16g
+#SBATCH --tmp=16g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=howex118@umn.edu
+
+helpFunction() {
+    echo ""
+    echo "Usage: $0 -i input_dir -d DNA_Reference"
+    echo -e "\t-i Folder with input files in fastq(.gz) format"
+    echo -e "\t-d DNA reference file directory path"
+    exit 1 # Exit script after printing help
+}
+
+while getopts "i:d:" ARGS; do
+    case "$ARGS" in
+    i) input_dir=$OPTARG ;;
+    d) DNA_Reference=$OPTARG ;;
+    ?) helpFunction ;; # Print helpFunction in case parameter is non-existent
+    esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$input_dir" ] || [ -z "$DNA_Reference" ]; then
+    echo "Some or all of the parameters are empty"
+    helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$input_directory"
+echo "$DNA_Reference"
+
+### Checking input_dir
+
+if [ ! -d "${input_dir}" ] && [ -x "${input_dir}" ]; then
+    echo -e "Input directory not provided, Aborting"
+    echo ""
+    helpFunction
+    exit
+
+else
+    echo -e "\n#\tContents of input directory: ${input_dir}"
+    echo -e "\n#============================================================\n"
+    INfile=$(find "${input_dir}" -iname "*_R*_001.fastq.*" | awk -F . '{print $NF}' | head -1)
+    forward_read_filecount=$(find "${input_dir}" -name "*_R1_001.fastq.${INfile}" | wc -l)
+
+    #
+    forward_read_filesize=$(find "${input_dir}" -name "*_R1_001.fastq.${INfile}" -print0 | du --files0-from=- -ch | grep "total" | cut -f1)
+
+    #
+    echo -e "\t-> Number of read files: $forward_read_filecount"
+    echo -e "\t-> Size of read files:   $forward_read_filesize"
+    echo -e "\n#============================================================\n"
+fi
+
+### Setting up input directory and other variables for child processes
+
+export INPDIR="${input_dir}"
+export EXT="${INfile}"
+
+### Adapter trimming and quality filtering of the raw reads
+
+module load cutadapt
+
+mkdir -p "${WORKDIR}/tntrimmed"
+mkdir -p "${WORKDIR}/finaltrimmed"
+
+cut_adapt() {
+    # Cutadapt function
+    # Calls external cutadapt script
+    Tnseq_cutadapt.sh
+
+}
+
+cut_adapt
+
+# module load cutadapt
+# module load bowtie2
+
+# echo "Step 1: Transfer files and build directories"
+# # Create a folder in your home directory and link to the TNFOLDER variable
+# # Link your DNA reference to the DNAREFERENCE variable
+# # Link your Data files location to the DATALOCATION variable
+# # Link your SAMtoWig python file location to SAMTOWIG variable
+# TNFOLDER="/home/baughna/howex118/Tnseqtest"
+# DATALOCATION="/home/baughna/howex118/YMTnTestFiles"
+# DNAREFERENCE="/home/baughna/howex118/Reference_genomes_anns/H37Rv_Reference_Genome_NC_000962.3.fasta"
+# SAMTOWIG="/home/baughna/howex118/SAMtoWig_Py/Wig_from_Fastq_tnseq_MDH.py"
+
+# cp -t $TNFOLDER $DNAREFERENCE
+# cp -t $TNFOLDER $DATALOCATION/*_R1_001.fastq
+# cp -t $TNFOLDER $SAMTOWIG
+
+# cd $TNFOLDER
+
+# mkdir tntrim
+# mkdir trimmed_reads
+# mkdir Index
+# mkdir SAMfiles
+
+# # Copy DNA Reference into TnFolder for running custom SAM to WIG Python Script
+
+# echo "Step 2: Create sample list"
+
+# ls *_R1_001.fastq | cut -f1 -d "." >samples_names_Tnseq.txt
+
+# echo "Step 3: Trimming sequence reads for transposon sequences and illumina adapters from files"
+
+# for sample in $(cat samples_names_Tnseq.txt); do
+
+#     echo "On sample: $sample"
+#     echo "Trimming Transposons for: $sample"
+
+#     cutadapt -g CCGGGGACTTATCAGCCAACCTGT \
+#         --discard-untrimmed \
+#         --cores=20 \
+#         -o $TNFOLDER/tntrim/${sample}_Tntrimmed.fastq \
+#         $TNFOLDER/${sample}.fastq
+
+#     echo "Trimming Adapters for: $sample"
+
+#     cutadapt -a GATCCCACTAGTGTCGACACCAGTCTC --minimum-length=18 \
+#         --cores=20 \
+#         -o $TNFOLDER/trimmed_reads/${sample}_FinalTrim.fastq \
+#         $TNFOLDER/tntrim/${sample}_Tntrimmed.fastq
+
+# done
+
+# echo "Step 4: Building Mycobacterium Reference and aligning reads to genome"
+
+# bowtie2-build -f $DNAREFERENCE Index/Mycobacteria_Index
+
+# for sample in $(cat samples_names_Tnseq.txt); do
+#     echo "Aligning sample: $sample"
+#     bowtie2 -q -sam -x Index/Mycobacteria_Index $TNFOLDER/trimmed_reads/${sample}_FinalTrim.fastq -S SAMfiles/${sample}_Bowtie2.sam
+
+# done
+
+# echo "Step 5: Converting SAM file to Wig file for running in TRANSIT"
+# # Purge bowtie2 and cutadapt packages to avoid conflict errors with biopython module
+# module purge
+# module load biopython
+
+# REFERENCEFILENAME="$(basename $DNAREFERENCE)"
+# echo "$REFERENCEFILENAME"
+# export REFERENCEFILENAME
+
+# python $TNFOLDER/Wig_from_Fastq_tnseq_MDH.py
---
+++ Wig_from_Fastq_tnseq_MDH.py
@@ -0,0 +1,79 @@
+from Bio import SeqIO
+import os
+
+ref_genome_bowtie_index = "NC_000962.3"  # Bowtie index name
+# Reference genome FASTA file
+ref_genome_fasta = os.environ["REFERENCEFILENAME"]
+print(ref_genome_fasta)
+folder_name = "SAMfiles"  # Folder containing SAM files
+
+TA_sites = []
+filename = ref_genome_fasta
+count = 0
+for record in SeqIO.parse(filename, "fasta"):
+    for i in record.seq:
+        if i == "T":
+            T_found = True
+            T_pos = count
+        elif i == "A":
+            if T_found == True:
+                TA_sites.append(T_pos)
+                T_found = False
+        else:
+            T_found = False
+        count += 1
+
+# Make dictionary of possible TA sites in reference
+TA_dict = dict.fromkeys(TA_sites, 0)
+
+# Make list of SAM files
+data_file_names = os.listdir(folder_name)
+
+files = []
+for i in data_file_names:
+    if i[-3:] == "sam":
+        files.append((folder_name + "/" + i))
+
+for i in files:
+    # Make dictionary of possible TA sites in reference
+    TA_dict = dict.fromkeys(TA_sites, 0)
+    fname = i
+    save_name = (fname[:-3] + "wig")
+    save_file = open(save_name, "w")
+    header = ("#", '\n', "variableStep chrom=" + ref_genome_bowtie_index, '\n')
+    save_file.write(''.join(map(str, header)))
+    newtab = '\t'
+    newline = '\n'
+    with open(fname) as input:
+        next(input)
+        next(input)
+        next(input)
+        for line in input:
+            xx = line.split('\t')
+            # xx[0] - read name
+            #xx[1] - orientation
+            # xx[2] - ref genome
+            # xx[3] - map position
+            #xx[9] - sequence
+            #xx[10] - q-score
+            seq_len = len(xx[9])
+            if xx[1] == "16":
+                ins_pos_temp = int(xx[3]) + seq_len - 3
+            elif xx[1] == "0":
+                ins_pos_temp = int(xx[3]) - 1
+            else:
+                continue
+            try:
+                TA_dict[ins_pos_temp] += 1
+            except KeyError:
+                pass
+    ordered_keys = sorted(TA_dict)
+    ordered_values = []
+    for j in ordered_keys:
+        save_file.write(str(j))
+        save_file.write(newtab)
+        ordered_values.append(TA_dict[j])
+        save_file.write(str(TA_dict[j]))
+        save_file.write(newline)
+    print("done with " + fname)
+    save_file.close()
