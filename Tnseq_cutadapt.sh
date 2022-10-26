3f782b38d93c8b3a4fe1ce676abb27cacc24d839
Michael H01~ <howex118@umn.edu>
2022-10-26 17:27:39 -0500

    Update

--- Tnseq_cutadapt.sh
+++ Tnseq_cutadapt.sh
@@ -11,18 +11,21 @@ command -v cutadapt >/dev/null 2>&1 || {
 # Defining process variables
 
 cutadapt_dir="${BASEDIR}/tntrimmed"
+echo "$cutadapt_dir"
 final_cutadapt_dir="${BASEDIR}/finaltrimmed"
-
+echo "$final_cutadapt_dir"
 # Find the file and execute transposon trimming and adapter trimming
 
-for file in $(find "${INPDIR}" -maxdepth 1 -type l -name "*_001.fastq${EXT}"); do
+for file in $(find "${INPDIR}" -maxdepth 1 -name "*_001.fastq${EXT}"); do
 
     if [ ! -L "${file}" ]; then
         echo "Error: No *.fastq'${EXT}' files found in ${INPDIR}"
         break
     else
-        In_names=$(basename ${file} | sed "*_001.fastq${EXT}//")
-        Out_tntrimmed="${In_names}_tntrimmed.fastq"
+        In_name=$(basename ${file} | sed "*_001.fastq${EXT}//")
+        echo "$In_name"
+        Out_tntrimmed="${In_name}_tntrimmed.fastq"
+        echo "$Out_tntrimmed"
 
         echo "On sample: $file"
         echo "Trimming Transposons for: $file"
@@ -42,8 +45,8 @@ for file in $(find "${cutadapt_dir}" -maxdepth 1 -type l -name "*_001.fastq"); d
         echo "Error: No *.fastq' trimmed files found in ${final_cutadapt_dir}"
         break
     else
-        In_names_final=$(basename ${file} | sed "*_001.fastq${EXT}//")
-        Out_tntrimmed_final="${In_names_final}_FinalTrim.fastq"
+        In_name_final=$(basename ${file} | sed "*_001.fastq${EXT}//")
+        Out_tntrimmed_final="${In_name_final}_FinalTrim.fastq"
 
         echo "On sample: $file"
         echo "Trimming Adapters for: $file"
