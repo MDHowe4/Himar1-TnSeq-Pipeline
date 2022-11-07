580c8d66d799dd0670f69b1b2cca33a4c3c72b64
Michael H01~ <howex118@umn.edu>
2022-11-07 11:40:34 -0600

    Update

--- Tnseq_Analysis_Script.sh
+++ Tnseq_Analysis_Script.sh
@@ -7,6 +7,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=howex118@umn.edu
 
+start_time='date +%s'
+echo "start time:" $start_time
+
 cd ${PWD}
 
 helpFunction() {
@@ -31,50 +34,51 @@ if [ -z "$input_dir" ] || [ -z "$DNA_reference" ]; then
     helpFunction
 fi
 
-# Begin script in case all parameters are correct
-echo "$input_dir"
-echo "$DNA_reference"
-
-### Checking input_dir
+# Print helpFunction in case parameters are empty
 
 if [ ! -d "${input_dir}" ] && [ -x "${DNA_reference}" ]; then
-    echo -e "Input directory not provided, Aborting"
+    echo -e "Input directory or DNA reference not provided, Aborting"
     echo ""
     helpFunction
     exit
 
 else
-    echo -e "\n#\tContents of input directory: ${input_dir}"
-    echo -e "\n#============================================================\n"
+    echo -e "\n#\t----------Contents of input directory----------: ${input_dir}"
+    echo -e "\n#---------------------------------------------------\n"
     INfile=$(find "${input_dir}" -iname "*_R*_001.fastq.*" | awk -F . '{print "."$NF}' | head -1)
 
     forward_read_filecount=$(find "${input_dir}" -name "*_R1_001.fastq${INfile}" | wc -l)
-
-    #
     forward_read_filesize=$(find "${input_dir}" -name "*_R1_001.fastq${INfile}" -print0 | du --files0-from=- -ch | grep "total" | cut -f1)
 
-    #
     echo -e "\t-> Number of read files: $forward_read_filecount"
     echo -e "\t-> Size of read files:   $forward_read_filesize"
-    echo -e "\n#============================================================\n"
+    echo -e "\n#---------------------------------------------------\n"
 fi
 
-echo "${INfile}"
-### Setting up input directory and base directory and other variables for child processes
+#################################################################
+#################        SCRIPT BEGIN       #####################
+#################################################################
+
+# Begin script in case all parameters are correct
+echo "$input_dir"
+echo "$DNA_reference"
+
+# Setting up input directory and base directory and other variables for child processes
 echo "Script executed from: ${PWD}"
-export BASEDIR=${PWD}
 echo "Script location: ${BASEDIR}"
 
+export BASEDIR=${PWD}
 export INPDIR="${input_dir}"
 export EXT="${INfile}"
 export DNAREFERENCE="${DNA_reference}"
-echo "${DNAREFERENCE}"
-### Transposon and adapter trimming and of the raw reads
-### Genome idexing/alignment
+
+# Transposon and adapter trimming and of the raw reads
+# Genome idexing/alignment
 
 module load cutadapt
 module load bowtie2
 
+echo ['date +"%Y-%m-%d %H:%M:%S"']"----------Creating Directories----------"
 mkdir -p "${BASEDIR}/tntrimmed"
 mkdir -p "${BASEDIR}/finaltrimmed"
 mkdir -p "${BASEDIR}/index"
@@ -83,27 +87,30 @@ mkdir -p "${BASEDIR}/SAMfiles"
 run_tnseq_functions() {
     # Cutadapt function
     # Calls external cutadapt script from within base directory
+    echo ['date +"%Y-%m-%d %H:%M:%S"']"----------Running Cutadapt on Samples----------"
     ${BASEDIR}/Tnseq_cutadapt.sh
     # Calls external bowtie2 script from within base directory
+    echo ['date +"%Y-%m-%d %H:%M:%S"']"----------Running Bowtie2 on Samples----------"
     ${BASEDIR}/Tnseq_Bowtie2.sh
 
 }
 
 run_tnseq_functions
 
-echo "Converting SAM file to Wig file for running in TRANSIT"
+echo ['date +"%Y-%m-%d %H:%M:%S"']"----------Coverting SAM File to WIG File----------"
+# Converting SAM file to Wig file for running in TRANSIT
 # Purge bowtie2 and cutadapt packages to avoid conflict errors with biopython module
 
 module purge
 module load biopython
+# Export file names for .wig file output
 
 REFERENCEFILENAME="${DNAREFERENCE}"
 REFERENCENAME="$(basename $DNAREFERENCE)"
-echo "$REFERENCEFILENAME"
-echo "$REFERENCENAME"
 export REFERENCEFILENAME
 export REFERENCENAME
 
 python ${BASEDIR}/Wig_from_Fastq.py
-echo "Hello"
-# End of Tn-seq analysis script
+
+echo ['date +"%Y-%m-%d %H:%M:%S"']"----------End of Tn-seq preprocessing Script----------"
+# End of Tn-seq preprocessing script
